b24f709579f11439dfaea2850bef7d4ad547b3d2
Namrata113 <gurbaninamrata01@gmail.com>
2023-09-07 14:28:54 +0530

    new message

---
+++ cal.js
@@ -0,0 +1,10 @@
+
+console.log 
+let num1 = Number(prompt('enter first value'));
+let num2 = Number(prompt('enter second value'));
+
+let result = num1+num2;
+
+alert(' the result is ' + result +'the best result');  
+
+int 
